ccdffdb3bc5cddfc2c69c12c4ab864229888ec75
Akash Kumar <151871443+Akki789@users.noreply.github.com>
2026-08-11 10:19:06 +0530

    Time: 4 ms (32.86%), Space: 65.1 MB (59.74%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit =0;
+    let bestBuy = prices[0];
+
+    for(let i=0; i<prices.length; i++){
+        if(prices[i]>bestBuy){
+            maxProfit = Math.max(maxProfit, prices[i]-bestBuy);
+        }
+        bestBuy = Math.min(prices[i], bestBuy)
+    }
+    return maxProfit;
+};
