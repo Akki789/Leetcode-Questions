3a4043a160a691c7064f16c4a6deaa176db2edd4
Akash Kumar <151871443+Akki789@users.noreply.github.com>
2026-08-14 01:33:42 +0530

    Time: 30 ms (74.29%), Space: 59.3 MB (70.99%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,15 +3,18 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let count = new Array(26).fill(0);
-
-    for (let ch of s) {
-        count[ch.charCodeAt(0) - 97]++;
+    const charCount = {};
+    
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        charCount[char] = (charCount[char] || 0) + 1;
     }
 
     for (let i = 0; i < s.length; i++) {
-        if (count[s[i].charCodeAt(0) - 97] === 1) return i;
+        if (charCount[s[i]] === 1) {
+            return i;
+        }
     }
-
+    
     return -1;
 };
